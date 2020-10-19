12c906616127ed4f03fd2d6b95e93a676f3fb8fe
Cole Anderson <cole.a307@stu.nebo.edu>
2020-10-19 12:38:14 -0600

    Finish Proto 2 Stm

--- Prototype 2 - Animal Stampede/Assets/Scripts/OutOfBounds.cs
+++ Prototype 2 - Animal Stampede/Assets/Scripts/OutOfBounds.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 public class OutOfBounds : MonoBehaviour
 {
 
-    public float topBounds = -15;
-    public float lowerBounds = 35;
+    public float topBounds = 35.05f;
+    public float lowerBounds = -15.0f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
     //if the game object goes past the topbounds limit it will be deleted 
-      if(transform.position.z < topBounds) 
+      if(transform.position.z > topBounds) 
       {
         Destroy(gameObject);
       }
       
     //if the game object goes past the lowerbounds limit it will be deleted
-      if(transform.position.z < lowerBounds) 
+      else if(transform.position.z < lowerBounds) 
       {
         Destroy(gameObject);
+        Debug.Log("GAME OVER");
       }
     }
 }
