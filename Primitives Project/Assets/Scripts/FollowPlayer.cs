4f59826f4276d2b4814b75605d895c8c55a9e400
Cole Anderson <cole.a307@stu.nebo.edu>
2020-12-04 12:41:30 -0700

    My Game Player Movement

---
+++ Primitives Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+public GameObject player;
+private Vector3 offset = new Vector3(0, 6, -12);
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes the camera match the players movement and position
+        transform.position = player.transform.position + offset;
+        transform.rotation = player.transform.rotation;
+    }
+}
+
--- Primitives Project/Assets/Scripts/PlayerController.cs
+++ Primitives Project/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 
 {
-    private float speed = 20;
-    private float jumpForce; 
+    private float Speed = 30; 
+    private float NormalSpeed = 30;
+    private float RunSpeed = 40;
+    private float jumpForce = 15; 
     private Rigidbody playerRb;
+    private  float horizontalInput;
+    private  float forwardInput;
+    private float turnSpeed = 60; 
+    public bool isOnGround = true;
+    public bool gameOver = false; 
+    public bool isRunning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +26,45 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space))  
+       horizontalInput = Input.GetAxis("Horizontal");
+
+       forwardInput = Input.GetAxis("Vertical");
+     
+     
+       transform.Translate(Vector3.forward * Time.deltaTime * Speed * forwardInput);
+     
+       transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+          
+          
+     if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
        {
-           playerRb.AddForce(Vector3.up * jumpForce);
+         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+         isOnGround = false;
+       } 
+
+     if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+       {
+             isRunning = true;
+             Speed = RunSpeed;
+
        }
+        else 
+        {
+ 
+             isRunning = false;
+             Speed = NormalSpeed;
+        }
+
+       }
+   
+
+      private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
+
+   
 }
