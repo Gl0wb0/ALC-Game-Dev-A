60e1a9e40e6ba1a47521561e2e37dacb52668d9c
Cole Anderson <cole.a307@stu.nebo.edu>
2020-12-16 12:39:28 -0700

    Sumo Finishings and the start of soccer

--- SumoAI/Assets/Scripts/PlayerController.cs
+++ SumoAI/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb; 
     public float speed = 5.0f; 
     private GameObject focalPoint; 
+    public bool hasPowerup; 
+    private float PowerupStrength = 15.0f;
+    public GameObject PowerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,39 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        PowerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true; 
+            Destroy(other.gameObject); 
+            StartCoroutine(PowerupCountdownRoutine());
+            PowerupIndicator.gameObject.SetActive(true); 
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        PowerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>(); 
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+          
+            Debug.Log("Player collided with" + collision.gameObject + " with poweup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * PowerupStrength, ForceMode.Impulse);
+        }
+
     }
 }
--- SumoAI/Assets/Scripts/Spawner.cs
+++ SumoAI/Assets/Scripts/Spawner.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9; 
+    public int enemyCount;
+    public int waveNumber = 1; 
 
     // Start is called before the first frame update
     void Start()
     {
-      Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
+      SpawnEnemyWave(waveNumber);
+      Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
+
+   void Update() 
+   {
+      enemyCount = FindObjectsOfType<TheEvilScript>().Length;
+
+      if (enemyCount == 0) 
+      {
+         waveNumber++;
+         SpawnEnemyWave(waveNumber); 
+         
+      }
+   }
 
    private Vector3 GenerateSpawnPosition ()
    {
@@ -19,5 +35,13 @@ public class Spawner : MonoBehaviour
       float spawnPosZ = Random.Range(-spawnRange, spawnRange);
       Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
       return randomPos; 
+   }
+
+   void SpawnEnemyWave(int enemiesToSpawn)
+   {
+      for (int i = 0; 1 < enemiesToSpawn; i++)
+      {
+         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
+      }
    }
 }
